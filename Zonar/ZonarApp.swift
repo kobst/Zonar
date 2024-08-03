fa879863af1f11d252a2ae78193092ec5c5ca76e
Edward Han <edwrdhn@gmail.com>
2024-08-03 13:43:48 -0400

    Initial Commit

---
+++ Zonar/ZonarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZonarApp.swift
+//  Zonar
+//
+//  Created by Edward Han on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ZonarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
